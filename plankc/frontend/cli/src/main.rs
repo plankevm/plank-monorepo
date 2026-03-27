0c18fb1ffa4b8a8bcce1f9a167a837b5e9a76f6e
philogy <philippe@dumo.net>
2026-03-27 15:48:28 +0000

    remove already-ssa

--- plankc/frontend/cli/src/main.rs
+++ plankc/frontend/cli/src/main.rs
@@ -25,9 +25,6 @@ struct Args {
     #[arg(short = 'O', long = "optimize", help = OPTIMIZE_HELP, value_parser = parse_optimizations_string)]
     optimize: Option<String>,
 
-    #[arg(long = "already-ssa")]
-    already_ssa: bool,
-
     #[arg(long = "module-name")]
     module_name: Option<String>,
 
@@ -103,7 +100,7 @@ fn main() {
         driver.render_diagnostics_and_exit();
     }
 
-    let bytecode = driver.emit_bytecode(&mir, args.already_ssa, args.optimize.as_deref());
+    let bytecode = driver.emit_bytecode(&mir, args.optimize.as_deref());
 
     print!("0x");
     for byte in bytecode {
--- plankc/frontend/driver/src/lib.rs
+++ plankc/frontend/driver/src/lib.rs
@@ -50,19 +50,10 @@ impl<'a, F: SourceFs> Driver<'a, F> {
         plank_hir_eval::evaluate(hir, &mut self.session)
     }
 
-    pub fn emit_bytecode(
-        &self,
-        mir: &plank_mir::Mir,
-        already_ssa: bool,
-        optimizations: Option<&str>,
-    ) -> Vec<u8> {
+    pub fn emit_bytecode(&self, mir: &plank_mir::Mir, optimizations: Option<&str>) -> Vec<u8> {
         let mut program = plank_mir_lower::lower(mir, &self.big_nums);
         let mut pass_manager = PassManager::new(&mut program);
-        if already_ssa {
-            pass_manager.run_legalize().expect("illegal IR pre-ssa");
-        } else {
-            pass_manager.run_ssa_transform();
-        }
+        pass_manager.run_ssa_transform();
         if let Some(passes) = optimizations {
             pass_manager.run_optimizations(passes);
         }
