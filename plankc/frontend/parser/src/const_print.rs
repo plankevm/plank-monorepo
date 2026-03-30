c706ecf24994e68c0784f1ff10f5d49f83651be7
philogy <philippe@dumo.net>
2026-03-30 11:47:24 +0100

    remove magic number

--- plankc/frontend/parser/src/const_print.rs
+++ plankc/frontend/parser/src/const_print.rs
@@ -1,9 +1,11 @@
-pub const fn const_num_to_str(buf: &mut [u8; 20], mut x: u64) -> &str {
+const REPR_BUF_SIZE: usize = 20;
+
+pub const fn const_num_to_str(buf: &mut [u8; REPR_BUF_SIZE], mut x: u64) -> &str {
     // Safety: 10^20 > 2^64
     unsafe {
-        buf[19] = b'0';
+        buf[REPR_BUF_SIZE - 1] = b'0';
 
-        let mut i = 20;
+        let mut i = REPR_BUF_SIZE;
         while x > 0 {
             i -= 1;
             buf[i] = (x % 10) as u8 + b'0';
@@ -11,7 +13,7 @@ pub const fn const_num_to_str(buf: &mut [u8; 20], mut x: u64) -> &str {
         }
 
         let ptr: *mut u8 = buf.as_mut_ptr();
-        let bytes = core::slice::from_raw_parts_mut(ptr.add(i), 20 - i);
+        let bytes = core::slice::from_raw_parts_mut(ptr.add(i), REPR_BUF_SIZE - i);
         str::from_utf8_unchecked(bytes)
     }
 }
@@ -20,7 +22,7 @@ pub const fn const_assert_eq(x: usize, y: usize) {
     if x == y {
         return;
     }
-    let mut xbuf = [0u8; 20];
+    let mut xbuf = [0u8; REPR_BUF_SIZE];
     let xs = const_num_to_str(&mut xbuf, x as u64);
     panic!("{}", xs);
 }
