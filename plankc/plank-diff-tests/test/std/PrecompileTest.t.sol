a5e6dc24a759e9aa1f8c5467e351b1c969938470
siosw <git@siosw.net>
2026-05-08 23:36:01 +0200

    simplify test

--- plankc/plank-diff-tests/test/std/PrecompileTest.t.sol
+++ plankc/plank-diff-tests/test/std/PrecompileTest.t.sol
@@ -2,22 +2,14 @@
 pragma solidity ^0.8.0;
 
 import {BaseTest} from "../BaseTest.sol";
-import {PrecompileTest} from "src/std/PrecompileTest.sol";
 import {console} from "forge-std/console.sol";
 
 contract PrecompileTestTest is BaseTest {
-    PrecompileTest solRef;
     address plankImpl = makeAddr("plank-implementation");
 
     function setUp() public {
-        solRef = new PrecompileTest();
-
         bytes memory plankCode = plank("src/std/precompile_test.plk");
         plankImpl = deployCode(plankCode);
-    }
-
-    function assertCallEq(bytes memory data) internal {
-        assertCallEq(address(solRef), plankImpl, data);
     }
 
     function test_ecrecover() public {
@@ -43,15 +35,9 @@ contract PrecompileTestTest is BaseTest {
             bytes32(out),
             0x0000000000000000000000007156526fbd7a3c72969b54f64e42c10fbb768c8a
         );
-
-        assertCallEq(
-            abi.encodeWithSignature(
-                "ecrecoverExt(bytes32,uint8,bytes32,bytes32)",
-                hash,
-                v,
-                r,
-                s
-            )
+        assertEq(
+            bytes32(out),
+            bytes32(uint256(uint160(ecrecover(hash, v, r, s))))
         );
     }
 
@@ -69,50 +55,62 @@ contract PrecompileTestTest is BaseTest {
         assert(succ);
         assertEq(out.length, 32);
         assertEq(bytes32(out), bytes32(0));
-
-        assertCallEq(
-            abi.encodeWithSignature(
-                "ecrecoverExt(bytes32,uint8,bytes32,bytes32)",
-                bytes32(0),
-                0,
-                bytes32(0),
-                bytes32(0)
-            )
+        assertEq(
+            bytes32(out),
+            bytes32(uint256(uint160(ecrecover(0, 0, 0, 0))))
         );
     }
 
     function test_sha256() public {
-        assertCallEq(
-            abi.encodeWithSignature("sha256Ext(bytes)", abi.encode(0xdeadbeef))
+        bytes memory data = abi.encode(0xdeadbeef);
+        (bool succ, bytes memory out) = plankImpl.call(
+            abi.encodeWithSignature("sha256Ext(bytes)", data)
         );
+        assertEq(succ, true);
+        assertEq(bytes32(out), sha256(data));
     }
 
     function test_fuzz_sha256(bytes memory data) public {
-        assertCallEq(abi.encodeWithSignature("sha256Ext(bytes)", data));
+        (bool succ, bytes memory out) = plankImpl.call(
+            abi.encodeWithSignature("sha256Ext(bytes)", data)
+        );
+        assertEq(succ, true);
+        assertEq(bytes32(out), sha256(data));
     }
 
     function test_ripemd160() public {
-        assertCallEq(
-            abi.encodeWithSignature(
-                "ripemd160Ext(bytes)",
-                abi.encode(0xdeadbeef)
-            )
+        bytes memory data = abi.encode(0xdeadbeef);
+        (bool succ, bytes memory out) = plankImpl.call(
+            abi.encodeWithSignature("ripemd160Ext(bytes)", data)
         );
+        assertEq(succ, true);
+        assertEq(bytes32(out), bytes32(uint256(uint160(ripemd160(data)))));
     }
 
     function test_fuzz_ripemd160(bytes memory data) public {
-        assertCallEq(abi.encodeWithSignature("ripemd160Ext(bytes)", data));
+        (bool succ, bytes memory out) = plankImpl.call(
+            abi.encodeWithSignature("ripemd160Ext(bytes)", data)
+        );
+        assertEq(succ, true);
+        assertEq(bytes32(out), bytes32(uint256(uint160(ripemd160(data)))));
     }
 
     function test_modexp() public {
-        assertCallEq(
+        bytes memory base = abi.encode(0x02);
+        bytes memory exp = abi.encode(0x02);
+        bytes memory mod = abi.encode(0xff);
+
+        (bool succ, bytes memory out) = plankImpl.call(
             abi.encodeWithSignature(
                 "modexpExt(bytes,bytes,bytes)",
-                abi.encode(0x02),
-                abi.encode(0x02),
-                abi.encode(0xff)
+                base,
+                exp,
+                mod
             )
         );
+
+        assertTrue(succ);
+        assertEq(abi.decode(out, (bytes)), modexp(base, exp, mod));
     }
 
     function test_fuzz_modexp(
@@ -120,13 +118,36 @@ contract PrecompileTestTest is BaseTest {
         bytes memory exp,
         bytes memory mod
     ) public {
-        assertCallEq(
+        (bool succ, bytes memory out) = plankImpl.call(
             abi.encodeWithSignature(
                 "modexpExt(bytes,bytes,bytes)",
-                abi.encode(base),
-                abi.encode(exp),
-                abi.encode(mod)
+                base,
+                exp,
+                mod
             )
         );
+
+        assertTrue(succ);
+        assertEq(abi.decode(out, (bytes)), modexp(base, exp, mod));
+    }
+
+    function modexp(
+        bytes memory base,
+        bytes memory exp,
+        bytes memory mod
+    ) internal view returns (bytes memory) {
+        (bool success, bytes memory out) = address(5).staticcall(
+            bytes.concat(
+                bytes32(base.length),
+                bytes32(exp.length),
+                bytes32(mod.length),
+                base,
+                exp,
+                mod
+            )
+        );
+
+        assert(success);
+        return out;
     }
 }
