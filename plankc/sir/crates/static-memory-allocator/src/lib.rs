8cd48ee2e7b60f6b61bafb6d8167ddff7ac6a214
philogy <philippe@dumo.net>
2026-05-14 17:04:48 +0100

    static memory allocator

--- plankc/sir/crates/static-memory-allocator/src/lib.rs
+++ plankc/sir/crates/static-memory-allocator/src/lib.rs
@@ -1,18 +1,66 @@
 use hashbrown::{HashMap, HashSet};
-use sir_data::StaticAllocId;
+use plank_core::list_of_lists::ListOfLists;
+use sir_data::{BasicBlockId, EthIRProgram, StaticAllocId};
+use sir_passes::AnalysesStore;
+use sir_stack_scheduling::stack::StackOps;
+use std::num::NonZero;
+
+mod noob;
+
+pub use noob::EvmStaticAllocator;
 
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct EvmMemAddr(u32);
+pub struct EvmMemAddr(NonZero<u32>);
+
+impl EvmMemAddr {
+    #[track_caller]
+    pub const fn new(offset: u32) -> EvmMemAddr {
+        EvmMemAddr(NonZero::new(!offset).expect("NonZero<u32> overflow"))
+    }
+
+    pub const fn get(self) -> u32 {
+        !self.0.get()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct DynFreePointer {
+    pub store_slot: EvmMemAddr,
+    pub start_value: EvmMemAddr,
+}
 
 #[derive(Debug, Clone)]
 pub struct Layout {
-    pub dyn_free_pointer_slot: Option<EvmMemAddr>,
+    pub dyn_free_pointer: Option<DynFreePointer>,
     pub switch_store: Option<EvmMemAddr>,
     pub alloc_start: HashMap<StaticAllocId, EvmMemAddr>,
     pub alloc_needs_zeroing: HashSet<StaticAllocId>,
 }
 
+pub enum EntrypointKind {
+    Init,
+    Run,
+}
+
 pub trait LayoutGenerator {
-    fn generate(&mut self) -> Layout;
+    fn generate(
+        &mut self,
+        program: &EthIRProgram,
+        analyses: &AnalysesStore,
+        entrypoint: EntrypointKind,
+        stack_ops: &ListOfLists<BasicBlockId, StackOps>,
+    ) -> Layout;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn evm_addr_rountrip() {
+        assert_eq!(EvmMemAddr::new(0).get(), 0);
+        assert_eq!(EvmMemAddr::new(1).get(), 1);
+        assert_eq!(EvmMemAddr::new(0xfffffffe).get(), 0xfffffffe);
+    }
 }
