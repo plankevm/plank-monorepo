a60153e12caee2cd0594636e304505d31fb128a0
Vincent Geddes <117534+vgeddes@users.noreply.github.com>
2026-06-21 15:25:09 +0200

    clippy

--- plankc/crates/plank-evm/src/spec.rs
+++ plankc/crates/plank-evm/src/spec.rs
@@ -8,12 +8,12 @@ pub enum EvmSpecId {
     Osaka,
 }
 
-impl Into<U256> for EvmSpecId {
-    fn into(self) -> U256 {
-        match self {
-            Self::Cancun => U256::from(0),
-            Self::Prague => U256::from(1),
-            Self::Osaka => U256::from(2),
+impl From<EvmSpecId> for U256 {
+	fn from(value: EvmSpecId) -> Self {
+		match value {
+            EvmSpecId::Cancun => U256::from(0),
+            EvmSpecId::Prague => U256::from(1),
+            EvmSpecId::Osaka => U256::from(2),
         }
-    }
+	}
 }
