28488292801f1d338cd3f6686b18224b0d5fb767
philogy <philippe@dumo.net>
2026-08-01 15:17:00 +0100

    basic tests

---
+++ plankc/plank-diff-tests/test/std/NewStorage.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseTest} from "../BaseTest.sol";
+
+contract NewStorageTest is BaseTest {
+    function test_storeAndLoadPackedNestedValue() public {
+        plankDeploy("src/std/new_storage_test.plk");
+    }
+}
