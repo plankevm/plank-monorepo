5c977b97abb5586259f3fe2f22823818d2d02112
philogy <philippe@dumo.net>
2026-08-01 15:45:38 +0100

    test layout

--- plankc/plank-diff-tests/test/std/NewStorage.t.sol
+++ plankc/plank-diff-tests/test/std/NewStorage.t.sol
@@ -7,4 +7,8 @@ contract NewStorageTest is BaseTest {
     function test_storeAndLoadPackedNestedValue() public {
         plankDeploy("src/std/new_storage_test.plk");
     }
+
+    function test_layoutV1() public {
+        plankDeploy("src/std/new_storage_layout_test.plk");
+    }
 }
