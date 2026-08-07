30e4a8d6486edfcbec6e27826765aaf3e03a0149
Oana Barbu <akakob@gmail.com>
2026-08-07 18:05:26 +0100

    add reachable functions

--- plankc/sir/crates/passes/src/analyses/reachability.rs
+++ plankc/sir/crates/passes/src/analyses/reachability.rs
@@ -1,6 +1,6 @@
 use crate::analyses::{AnalysesStore, cache::Analysis};
 use plank_core::DenseIndexSet;
-use sir_data::{BasicBlockId, EthIRProgram};
+use sir_data::{BasicBlockId, EthIRProgram, FunctionId, Operation};
 
 #[derive(Debug, Clone, Default)]
 pub struct ReachableBlocks {
@@ -24,5 +24,49 @@ impl ReachableBlocks {
 
     pub fn set_mut(&mut self) -> &mut DenseIndexSet<BasicBlockId> {
         &mut self.blocks
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct ReachableFunctions {
+    functions: DenseIndexSet<FunctionId>,
+    visited_blocks: DenseIndexSet<BasicBlockId>,
+    worklist: Vec<BasicBlockId>,
+}
+
+impl Analysis for ReachableFunctions {
+    fn compute(&mut self, program: &EthIRProgram, _store: &AnalysesStore) {
+        self.functions.clear();
+        self.visited_blocks.clear();
+        assert!(self.worklist.is_empty());
+        self.mark_reachable(program, program.init_entry);
+        if let Some(main) = program.main_entry {
+            self.mark_reachable(program, main);
+        }
+    }
+}
+
+impl ReachableFunctions {
+    fn mark_reachable(&mut self, program: &EthIRProgram, function: FunctionId) {
+        if !self.functions.add(function) {
+            return;
+        }
+
+        self.worklist.push(program.functions[function].entry());
+        while let Some(bb) = self.worklist.pop() {
+            if !self.visited_blocks.add(bb) {
+                continue;
+            }
+
+            for op in program.block(bb).operations() {
+                if let Operation::InternalCall(data) = op.op() {
+                    self.mark_reachable(program, data.function);
+                }
+            }
+
+            for succ in program.block(bb).successors() {
+                self.worklist.push(succ);
+            }
+        }
     }
 }
